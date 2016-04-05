ae1b5f3f9f7b2bdfc0f1b05be42503a7165234c3
Tomasz Jasek <tja@silversolutions.de>
2016-04-05 15:28:59 +0200

    override entity with disciplines

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -18,9 +18,67 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(name="disciplines", type="array")
+     *
+     * @var array
+     */
+    protected $disciplines;
+
     public function __construct()
     {
         parent::__construct();
-        // your own logic
+
+        $this->disciplines = array();
+    }
+
+    public function setDisciplines(array $disciplines)
+    {
+        $this->disciplines = array();
+
+        foreach ($disciplines as $discipline) {
+            $this->addDiscipline($discipline);
+        }
+
+        return $this;
+    }
+
+    public function addDiscipline($discipline)
+    {
+        if (!in_array($discipline, $this->disciplines, true)) {
+            $this->disciplines[] = $discipline;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Returns the user disciplines
+     *
+     * @return array The disciplines
+     */
+    public function getDisciplines()
+    {
+        return $this->disciplines;
+    }
+
+    /**
+     * @param string $discipline
+     *
+     * @return boolean
+     */
+    public function hasDiscipline($discipline)
+    {
+        return in_array($discipline, $this->getDisciplines(), true);
+    }
+
+    public function removeDiscipline($discipline)
+    {
+        if (false !== $key = array_search($discipline, $this->disciplines, true)) {
+            unset($this->disciplines[$key]);
+            $this->disciplines = array_values($this->disciplines);
+        }
+
+        return $this;
     }
 }
